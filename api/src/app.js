17d7d014e0bcbb179ca26934aedaa1c74e944a0f
Bryan Ramos Vargas <matematica_bryan@hotmail.com>
2022-10-20 12:49:48 -0500

    deploying backend v7 AccessControlAllowOrigin

--- api/src/app.js
+++ api/src/app.js
@@ -7,12 +7,12 @@ const routes = require('./routes/index.js');
 require('./db/db.js');
 
 const server = express();
-const URL = 'https://proyectpokemonbarv-r3i03g9kv-barv11.vercel.app/'
+const URL = 'https://https://proyectpokemonbarv.vercel.app'
 
 server.name = 'API';
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: 'mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
